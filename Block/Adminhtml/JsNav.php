665587fa26f4238aa5479135fb8f7da1da4856ea
Jaimin Sutariya <jsutariya@gmail.com>
2019-04-16 23:20:55 +0530

    Initial Release

---
+++ Block/Adminhtml/JsNav.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace JS\Launcher\Block\Adminhtml;
+
+use Magento\Backend\Block\Menu;
+use Magento\Backend\Block\AnchorRenderer;
+use Magento\Backend\Model\Menu\Config as MenuConfig;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Escaper;
+use Magento\Framework\Serialize\Serializer\Json;
+
+class JsNav extends \Magento\Backend\Block\Template
+{
+    /**
+     * @var Builder
+     */
+    private $menuBuilder;
+    /**
+     * @var AnchorRenderer
+     */
+    private $anchorRenderer;
+    /**
+     * @var MenuConfig
+     */
+    private $menuConfig;
+    /**
+     * @var \Magento\Backend\Model\UrlInterface
+     */
+    private $url;
+    /**
+     * @var Escaper
+     */
+    private $escaper;
+    /**
+     * @var \Magento\Backend\Model\Menu\Filter\IteratorFactory
+     */
+    private $iteratorFactory;
+    /**
+     * @var Json
+     */
+    private $jsonSerializer;
+    /**
+     * @var Context
+     */
+    private $context;
+
+    /**
+     * ActionLayoutRenderBefore constructor.
+     * @param Menu $menuBuilder
+     * @param AnchorRenderer $anchorRenderer
+     * @param MenuConfig $menuConfig
+     * @param \Magento\Backend\Model\UrlInterface $url
+     * @param Escaper $escaper
+     * @param \Magento\Backend\Model\Menu\Filter\IteratorFactory $iteratorFactory
+     * @param \Magento\Framework\App\Route\ConfigInterface|null $routeConfig
+     */
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        Menu $menuBuilder,
+        AnchorRenderer $anchorRenderer,
+        MenuConfig $menuConfig,
+        \Magento\Backend\Model\UrlInterface $url,
+        Escaper $escaper,
+        \Magento\Backend\Model\Menu\Filter\IteratorFactory $iteratorFactory,
+        \Magento\Framework\Serialize\Serializer\Json $jsonSerializer,
+        \Magento\Framework\App\Route\ConfigInterface $routeConfig = null, array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->menuBuilder = $menuBuilder;
+        $this->anchorRenderer = $anchorRenderer;
+        $this->menuConfig = $menuConfig;
+        $this->routeConfig = $routeConfig ?:
+            \Magento\Framework\App\ObjectManager::getInstance()
+                ->get(\Magento\Framework\App\Route\ConfigInterface::class);
+        $this->url = $url;
+        $this->escaper = $escaper;
+        $this->iteratorFactory = $iteratorFactory;
+        $this->jsonSerializer = $jsonSerializer;
+    }
+
+    /**
+     * Execute observer
+     *
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return void
+     */
+    public function getJson()
+    {
+        $menuItems = $this->renderMenu($this->menuConfig->getMenu(), 0);
+        $menuItemsJson = $this->jsonSerializer->serialize($menuItems);
+        return $menuItemsJson;
+    }
+
+    /**
+     * Replace Callback Secret Key
+     *
+     * @param string[] $match
+     * @return string
+     */
+    protected function _callbackSecretKey($match)
+    {
+        $routeId = $this->routeConfig->getRouteByFrontName($match[1]);
+        return \Magento\Backend\Model\UrlInterface::SECRET_KEY_PARAM_NAME . '/' . $this->url->getSecretKey(
+                $routeId ?: $match[1],
+                $match[2],
+                $match[3]
+            );
+    }
+
+    public function renderMenu($menu, $level = 0)
+    {
+        $parentArray = [];
+        /** @var $menuItem \Magento\Backend\Model\Menu\Item */
+        foreach ($this->_getMenuIterator($menu) as $menuItem) {
+            $menuArray = [];
+            $menuArray['label'] = $this->escaper->escapeHtml(__($menuItem->getTitle()));
+            $menuArray['url'] = preg_replace_callback(
+                '#' . \Magento\Backend\Model\UrlInterface::SECRET_KEY_PARAM_NAME . '/\$([^\/].*)/([^\/].*)/([^\$].*)\$#U',
+                [$this, '_callbackSecretKey'],
+                $menuItem->getUrl()
+            );
+            if ($menuItem->hasChildren()) {
+                $menuArray['children'] = $this->renderMenu($menuItem->getChildren(), $level + 1);
+            }
+            $parentArray[$menuItem->getTitle()] = $menuArray;
+        }
+        return $parentArray;
+    }
+
+    protected function _getMenuIterator($menu)
+    {
+        return $this->iteratorFactory->create(['iterator' => $menu->getIterator()]);
+    }
+
+    public function getCombinedCodes()
+    {
+        $first = $this->_scopeConfig->getValue('js_launcher/options/shortcut_code_first');
+        $second  = $this->_scopeConfig->getValue('js_launcher/options/shortcut_code_second');
+
+        $code = '17_77';        //default to ctrl-m
+        if(is_numeric($first) && is_numeric($second))
+        {
+            $code = $first . '_' . $second;
+        }
+
+        return $code;
+    }
+}
+
+
---
+++ README.md
@@ -0,0 +1,23 @@
+JS Launcher
+====================================
+
+## About
+
+JS Launcher is a menu navigation launcher module for Magento inspired by PulseStorm launcher in Magento 1.
+Current version only supports menu links in launcher. Configuration and Global search are coming soon with later versions.
+
+### Installation:
+Install using composer:
+
+`composer require jsutariya/launcher`
+
+`php bin/magento module:enable JS_Launcher`
+
+`php bin/magento setup:upgrade`
+
+`php bin/magento cache:flush`
+
+## Manual
+
+To launch navigation launcher in admin panel, press ctrl+m as default key combination. Key combination can be changed from admin panel configuration section.
+
